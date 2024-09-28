129435c98ec27b2adda821ebb30ba68262d51ed5
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-09-29 00:31:09 +0500

    design hero section and navbar

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "@/css/heroSection.css"
+import { Button } from './ui/button'
+function HeroSection() {
+  return (
+    <div id='heroSection'>
+        <h1 className='font-bold text-5xl'>Where Every Story Finds Its Audience</h1>
+        <p className='text-center my-5'>
+        Write, share, and explore powerful stories. Connect with readers who are ready to be inspired.
+        </p>
+
+        <Button variant={"outline"}>Get Started</Button>
+    </div>
+  )
+}
+
+export default HeroSection
